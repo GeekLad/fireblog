da823ff8bf5a8bb641dc64dc79ccccf507cd1353
Sierra <206525303@TFAYD.com>
2017-05-09 09:57:17 -0400

    Changed to DOMParser Created a WordPressImporter class with some basic getters

--- js/main.js
+++ js/main.js
@@ -1,4 +1,52 @@
 var xml2json;
+var WORDPRESS_PATHS = {
+    blogTitle: '/rss/channel/title',
+    blogUrl: '/rss/channel/link',
+    posts: '/rss/channel/item'
+};
+var WordPressImport = (function () {
+    function WordPressImport(xmlString) {
+        this._doc = (new DOMParser()).parseFromString(xmlString, "application/xml");
+    }
+    Object.defineProperty(WordPressImport.prototype, "blogTitle", {
+        get: function () {
+            return this._doc.evaluate(WORDPRESS_PATHS.blogTitle, this._doc, null, XPathResult.STRING_TYPE, null).stringValue;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(WordPressImport.prototype, "blogUrl", {
+        get: function () {
+            return this._doc.evaluate(WORDPRESS_PATHS.blogUrl, this._doc, null, XPathResult.STRING_TYPE, null).stringValue;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(WordPressImport.prototype, "posts", {
+        get: function () {
+            var posts = [];
+            var results = this._doc.evaluate(WORDPRESS_PATHS.posts, this._doc, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+            var post = results.iterateNext();
+            while (post) {
+                posts.push({
+                    id: Number(post["getElementsByTagName"]("post_id")[0].innerHTML),
+                    postTime: (new Date(post["getElementsByTagName"]("post_date")[0].innerHTML)).getTime(),
+                    type: post["getElementsByTagName"]("post_type")[0].innerHTML,
+                    originalUrl: post["getElementsByTagName"]("link")[0].innerHTML,
+                    path: post["getElementsByTagName"]("link")[0].innerHTML.replace(this.blogUrl, ""),
+                    title: post["getElementsByTagName"]("title")[0].innerHTML,
+                    content: post["getElementsByTagName"]("encoded")[0].innerHTML.replace(/^<!\[CDATA\[|\]\]>$/g, ""),
+                    excerpt: post["getElementsByTagName"]("encoded")[1].innerHTML.replace(/^<!\[CDATA\[|\]\]>$/g, "")
+                });
+                post = results.iterateNext();
+            }
+            return posts;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return WordPressImport;
+}());
 var StaticApp = (function () {
     function StaticApp() {
     }
@@ -9,7 +57,9 @@ var StaticApp = (function () {
         StaticApp._reader.readAsText(e.target.files[0]);
     };
     StaticApp._readFile = function () {
-        var data = JSON.parse(xml2json($.parseXML(StaticApp._reader.result)).replace(/^{\s+undefined/, "{"));
+        StaticApp._import = new WordPressImport(StaticApp._reader.result);
+        // var str = xml2json(doc);
+        // var data = JSON.parse(str.replace(/^{\sundefined/, "{"));
         document.write('<pre>DONE\n');
         //        document.write(data);
         document.write('</pre>');
--- js/main.ts
+++ js/main.ts
@@ -1,6 +1,61 @@
 var xml2json:(input:any) => any;
+
+interface Post {
+  id:number;
+  postTime:number;
+  originalUrl:string;
+  path:string;
+  type:string;
+  title:string;
+  content:string;
+  excerpt:string;
+}
+
+const WORDPRESS_PATHS = {
+  blogTitle: '/rss/channel/title',
+  blogUrl: '/rss/channel/link',
+  posts: '/rss/channel/item'
+}
+
+class WordPressImport {
+  private _doc:XMLDocument;
+
+  constructor(xmlString:string) {
+    this._doc = (new DOMParser()).parseFromString(xmlString, "application/xml");
+  }
+
+  public get blogTitle():string {
+    return this._doc.evaluate (WORDPRESS_PATHS.blogTitle, this._doc, null, XPathResult.STRING_TYPE, null).stringValue;
+  }
+
+  public get blogUrl():string {
+    return this._doc.evaluate (WORDPRESS_PATHS.blogUrl, this._doc, null, XPathResult.STRING_TYPE, null).stringValue;
+  }
+
+  public get posts():Array<Post> {
+    var posts:Array<Post> = [];
+    var results = this._doc.evaluate (WORDPRESS_PATHS.posts, this._doc, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+    var post = results.iterateNext();
+    while(post) {
+      posts.push({
+        id: Number(post["getElementsByTagName"]("post_id")[0].innerHTML),
+        postTime: (new Date(post["getElementsByTagName"]("post_date")[0].innerHTML)).getTime(),
+        type: post["getElementsByTagName"]("post_type")[0].innerHTML,
+        originalUrl: post["getElementsByTagName"]("link")[0].innerHTML,
+        path: post["getElementsByTagName"]("link")[0].innerHTML.replace(this.blogUrl, ""),
+        title: post["getElementsByTagName"]("title")[0].innerHTML,
+        content: post["getElementsByTagName"]("encoded")[0].innerHTML.replace(/^<!\[CDATA\[|\]\]>$/g, ""),
+        excerpt: post["getElementsByTagName"]("encoded")[1].innerHTML.replace(/^<!\[CDATA\[|\]\]>$/g, "")
+      });
+      post = results.iterateNext();
+    }
+    return posts;
+  }
+}
+
 class StaticApp {
   private static _reader:FileReader = new FileReader();
+  private static _import:WordPressImport;
 
   public static init() {
     StaticApp._reader.onload = StaticApp._readFile;
@@ -11,7 +66,10 @@ class StaticApp {
   }
 
   private static _readFile() {
-    var data = JSON.parse(xml2json($.parseXML(StaticApp._reader.result)).replace(/^{\s+undefined/, "{"));
+    StaticApp._import = new WordPressImport(StaticApp._reader.result);
+
+    // var str = xml2json(doc);
+    // var data = JSON.parse(str.replace(/^{\sundefined/, "{"));
     document.write('<pre>DONE\n');
 //        document.write(data);
     document.write('</pre>');
